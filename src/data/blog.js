023b115418ac490fafd2c218539e48d45eb4ba3e
Ivy Ostosh <io@ivys-mbp.lan>
2022-01-17 12:54:26 -0800

    Finished first blog.

--- src/data/blog.js
+++ src/data/blog.js
@@ -1,28 +1,28 @@
 /* eslint-disable max-len */
 // TODO Add a couple lines about each project
 const data = [
-  {
-    name: 'why_cs',
-    title: 'Why do I like Computer Science?',
-    subtitle: 'One question I often ask myself',
-    date: '2021-08-10',
-    desc:
-        'I originally wanted to answer the question "Why do I want to be a Software Engineer" first, but I believe this is a more fundamental question that I should start with ... ',
-    content: [
-      'This is a question I\'ve asked myself many times over the years. I haven\'t found an answer I\'m fully content with till this day. '
-      + 'So I will take a similar approach of how we approximate ILP solution using LP. I will try my best to articulate all the reasons I can think of. '
-      + 'Hopefully the day where P=NP will come soon, and everything will just naturally make perfect sense.',
-    ],
-  },
+  // {
+  //   name: 'why_cs',
+  //   title: 'Why do I like Computer Science?',
+  //   subtitle: 'One question I often ask myself',
+  //   date: '2021-08-10',
+  //   desc:
+  //       'I originally wanted to answer the question "Why do I want to be a Software Engineer" first, but I believe this is a more fundamental question that I should start with ... ',
+  //   content: [
+  //     'This is a question I\'ve asked myself many times over the years. I haven\'t found an answer I\'m fully content with till this day. '
+  //     + 'So I will take a similar approach of how we approximate ILP solution using LP. I will try my best to articulate all the reasons I can think of. '
+  //     + 'Hopefully the day where P=NP will come soon, and everything will just naturally make perfect sense.',
+  //   ],
+  // },
   {
     name: 'why_swe',
     title: 'My path to software engineering',
     subtitle: 'A journey of a thousand miles begins with a single step',
     date: '2022-01-16',
     desc:
-        'This is a question I\'ve asked myself many times over the years. I haven\'t found an answer I\'m fully content with till this day. '
-        + 'So I will take a similar approach of how we approximate ILP solution using LP. I will try my best to articulate all the reasons I can think of. '
-        + 'Hopefully the day where P=NP will come soon, and everything will just naturally make perfect sense ... ',
+        'I often wish I could be be more brave to pursue what I call my passion so this transition journey can be shorter. Over time, I learned to make peace with myself. '
+        + 'Yes, I\'m practical, risk averse, doubt myself often. I had all the qualities that tried the hardest to persuade me from starting a new career, '
+        + 'yet it never stopped me from making efforts towards learning Computer Science. So, one baby step after another, it eventually became clear to me Software Engineering is the career I would like to pursue for life ... ',
     content: [
       'This is a question I\'ve asked myself many times over the years. I haven\'t found an answer I\'m fully content with till this day. '
       + 'So I will take a similar approach of how we approximate ILP solution using LP. I will try my best to articulate all the reasons I can think of. '
